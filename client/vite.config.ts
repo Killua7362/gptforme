f7fd61d8dc987fab4b78205753d125af593c148c
Killua7362 <bhat7362@gmail.com>
2023-10-30 16:24:57 +0530

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig,loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [tsconfigPaths(),react()],
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,      
-        ws: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [tsconfigPaths(), react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_IP,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
+});
--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from 'express';
-
+import {GoogleAuth,} from 'google-auth-library'
 
 const app: Express = express();
-const port = process.env.PORT;
-
-
+const port = 5050;
+async function getexternalip(){
+  const auth = new GoogleAuth();
+  const authClient = await auth.getClient()
+}
+getexternalip()
 app.listen(port, () => {
-  console.log(`Server is running`);
+  console.log(`Server is running at port ${port}`);
 });
